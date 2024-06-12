4ca39024b6e1ad2df4afd7beb28dfb7c132a8404
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-11 22:20:56 -0500

    Brand Service Impl class is implemented

---
+++ sportscenter/src/main/java/com/ecommerce/sportscenter/service/impl/BrandServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ecommerce.sportscenter.service.impl;
+
+import com.ecommerce.sportscenter.entity.Brand;
+import com.ecommerce.sportscenter.model.BrandResponse;
+import com.ecommerce.sportscenter.repository.BrandRepository;
+import com.ecommerce.sportscenter.service.BrandService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Log4j2
+public class BrandServiceImpl implements BrandService {
+
+    private final BrandRepository brandRepository;
+
+    public BrandServiceImpl(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public List<BrandResponse> getAllBrands() {
+//        first logs the message
+        log.info("Fetching All Brands....!!!");
+//        fetch all the brand items from the repository
+        List<Brand> brandList = brandRepository.findAll();
+//        now use "stream()" operator to map with Response
+        List<BrandResponse> brandResponses = brandList.stream().map(this::convertToBrandResponse).collect(Collectors.toList());
+        log.info("Fetched All Brands ---- ");
+        return brandResponses;
+    }
+
+    private BrandResponse convertToBrandResponse(Brand brand) {
+        return BrandResponse.builder()
+                .id(brand.getId())
+                .name(brand.getName())
+                .build();
+    }
+}
