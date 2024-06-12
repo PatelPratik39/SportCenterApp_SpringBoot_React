80a5a8363203a0ccd8b5957f5b90ad2b975e69b7
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-12 10:47:01 -0500

    Frontend App is integrated with Backend app and Database

--- sportscenter/src/main/java/com/ecommerce/sportscenter/controller/ProductController.java
+++ sportscenter/src/main/java/com/ecommerce/sportscenter/controller/ProductController.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+
 public class ProductController {
 
     private final ProductService productService;
