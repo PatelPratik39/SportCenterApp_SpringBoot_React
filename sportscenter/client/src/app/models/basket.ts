37d4dff76480c63316965559efb7bea37610f5d2
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-13 15:21:28 -0500

    Reduux and created a basketSlice

---
+++ sportscenter/client/src/app/models/basket.ts
@@ -0,0 +1,21 @@
+export interface Basket {
+  id: string
+  items: BasketItem[]
+}
+
+export interface BasketItem {
+  id: number
+  name: string
+  description: string
+  price: number
+  pictureUrl: string
+  productBrand: string
+  productType: string
+  quantity: number
+}
+
+export interface BasketTotals {
+    shipping: number;
+    subtotal: number;
+    total: number;
+}
--- sportscenter/client/src/main.tsx
+++ sportscenter/client/src/main.tsx
@@ -7,12 +7,16 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
-import {router} from "./app/router/route.tsx";
+import { router } from "./app/router/route.tsx";
+import { Provider } from 'react-redux';
+import { store } from './app/store/ConfigureStores.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router= {router} />
-      {/* <App /> */}
-   
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+
+
   </React.StrictMode>,
 )
