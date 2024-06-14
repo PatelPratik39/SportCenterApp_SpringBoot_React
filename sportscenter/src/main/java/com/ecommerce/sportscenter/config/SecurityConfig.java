a563c91dfd59f93c31c8a60779619fa7aa153261
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-14 17:34:39 -0500

    Security config

--- sportscenter/src/main/java/com/ecommerce/sportscenter/config/SecurityConfig.java
+++ sportscenter/src/main/java/com/ecommerce/sportscenter/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.ecommerce.sportscenter.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,5 +41,10 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return authenticationManagerBuilder.getObject();
     }
 }
