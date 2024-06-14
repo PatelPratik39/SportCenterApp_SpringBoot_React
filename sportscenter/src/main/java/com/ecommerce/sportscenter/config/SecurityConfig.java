fd21e0d70605a1f7b3245f94265a5c4d25fff143
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-14 17:33:12 -0500

    Security config

---
+++ sportscenter/src/main/java/com/ecommerce/sportscenter/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.ecommerce.sportscenter.config;
+
+import com.ecommerce.sportscenter.security.JwtAuthenticationEntryPoint;
+import com.ecommerce.sportscenter.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter filter;
+
+    private final JwtAuthenticationEntryPoint entryPoint;
+
+    @Autowired
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    public SecurityConfig(JwtAuthenticationFilter filter, JwtAuthenticationEntryPoint entryPoint) {
+        this.filter = filter;
+        this.entryPoint = entryPoint;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer ::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/products").authenticated()
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().permitAll())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(entryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
