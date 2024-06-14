160a27ab5ceba0f7fd52d48f30fba289bcc55b5b
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-14 17:11:19 -0500

    Jwt Security configured

---
+++ sportscenter/src/main/java/com/ecommerce/sportscenter/security/JwtHelper.java
@@ -0,0 +1,73 @@
+package com.ecommerce.sportscenter.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtHelper {
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
+    // Ensure your secret key is at least 256 bits (32 characters) for HMAC-SHA256
+    private String secret = "MySuperSecretKeyForJWTThatIsLongEnough";
+
+
+
+    public String getUserNameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+    public Date getExpirationDateFromToken(String token){
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+    public String generateToken(UserDetails userDetails){
+        Map<String,Object> claims = new HashMap<>();
+        return  generateToken(claims, userDetails.getUsername());
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        final String username = getUserNameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    private String generateToken(Map<String, Object> claims, String subject) {
+//        convert the secret string key into a Key Object
+        Key hmacKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), SignatureAlgorithm.HS512.getJcaName());
+
+        return  Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(hmacKey,SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        Key hmacKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        return Jwts.parserBuilder()
+                .setSigningKey(hmacKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
